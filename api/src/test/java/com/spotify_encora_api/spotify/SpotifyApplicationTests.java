03c6e74eb6df2ba940c8d34ca3698ee75bf47dc7
Edwin.Saucedo <edwin.saucedo@ENCEDCHMOLT0569.local>
2024-12-10 13:23:24 -0600

    feat(api): initialize spring boot api project
    -Add lombok, jpa, and spring web dependencies:

---
+++ api/src/test/java/com/spotify_encora_api/spotify/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotify_encora_api.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
