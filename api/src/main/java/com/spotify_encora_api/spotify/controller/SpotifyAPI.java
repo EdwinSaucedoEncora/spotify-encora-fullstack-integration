a2c597d10b2debdc99df42c4989b78894829918e
Edwin.Saucedo <edwin.saucedo@ENCEDCHMOLT0569.local>
2025-01-03 02:50:12 -0600

    feat(api): add authentification flow and api implementation routes

--- api/src/main/java/com/spotify_encora_api/spotify/controller/SpotifyAPI.java
+++ api/src/main/java/com/spotify_encora_api/spotify/controller/SpotifyAPI.java
@@ -1,4 +1,23 @@
 package com.spotify_encora_api.spotify.controller;
 
+import com.spotify_encora_api.spotify.model.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 public interface SpotifyAPI {
+
+    @GetMapping("/search")
+    public ResponseEntity<SpotifySearch> search(@RequestParam String query, @RequestHeader(name = "Authorization") String token, @RequestHeader(name = "Refresh") String refresh);
+
+    @GetMapping("/me/top/artists")
+    public ResponseEntity<SpotifyPageableArtists> getUserTopArtists(@RequestHeader(name = "Authorization") String token, @RequestHeader(name = "Refresh") String refresh);
+
+    @GetMapping("/artists/{id}")
+    public ResponseEntity<SpotifyArtist> getArtistById(@RequestHeader(name = "Authorization") String token, @RequestHeader(name = "Refresh") String refresh, @PathVariable String id);
+
+    @GetMapping("/artists/{id}/top-tracks")
+    public  ResponseEntity<SpotifyArtistTopTracks> getArtistTopTracks(@RequestHeader(name = "Authorization") String token, @RequestHeader(name = "Refresh") String refresh, @PathVariable String id);
+
+    @GetMapping("/artists/{id}/albums")
+    public  ResponseEntity<SpotifyPageableAlbums> getArtistAlbums(@RequestHeader(name = "Authorization") String token, @RequestHeader(name = "Refresh") String refresh, @PathVariable String id);
 }
--- api/src/main/java/com/spotify_encora_api/spotify/controller/SpotifyAuth.java
+++ api/src/main/java/com/spotify_encora_api/spotify/controller/SpotifyAuth.java
@@ -1,22 +1,17 @@
 package com.spotify_encora_api.spotify.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
+import java.net.URISyntaxException;
 import java.util.Optional;
 
-@RequestMapping("/")
 public interface SpotifyAuth {
     @GetMapping("/auth/spotify")
-    public String auth(ModelMap model);
+    public String auth();
 
     @GetMapping("/oauth")
-    public ResponseEntity<String> oauth(@RequestParam(name = "error") Optional<String> error);
+    public String oauth(@RequestParam Optional<String> error, @RequestParam Optional<String> code, @RequestParam String state) throws URISyntaxException;
 }
